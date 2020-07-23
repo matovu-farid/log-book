84032adc2866d47834f974d3a93fda6474055ae2
faridmatovu <matovu90@gmail.com>
2020-07-23 23:24:15 +0300

    Implemented the table view

--- README.md
+++ README.md
@@ -3,3 +3,4 @@ first check the package.jon file and install all the dependencies
 
 run "node inde.js" in ypur terminal 
 open your local browser and input the foloowing url "http://127.0.0.1:3000/"
+Since googlr fonts were used, you may need to connect to web for better styling of fonts
--- index.js
+++ index.js
@@ -17,15 +17,15 @@ app.get('/styles');
 //List to store lod details
 const logCollection = [];
 
-
 //Handling the default request
-
 app.get('/', (req, res) => {
     res.render('index')});
    
 // When form is submitted
     app.post('/submitted', urlencodedParser,(req, res) => { 
        console.log(req.body);
-         res.render('submitted',{data:req.body});
+       logCollection.push(req.body);
+         res.render('submitted',{logArray:logCollection});
     });
+   
 app.listen(port, () => console.log(`Log-app listening on port ${port}!`));
